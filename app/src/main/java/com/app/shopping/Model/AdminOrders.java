88af9b5592854a5c66c415bafdcd7debea50e537
Kaushalya M.K.B <86149391+IT20118372@users.noreply.github.com>
2021-09-26 15:30:57 +0530

    Update AdminOrders.java

--- app/src/main/java/com/app/shopping/Model/AdminOrders.java
+++ app/src/main/java/com/app/shopping/Model/AdminOrders.java
@@ -15,6 +15,13 @@ public class AdminOrders {
         this.date = date;
         this.time = time;
         this.totalAmount = totalAmount;
+        
+        
+            /* this.pid = pid;
+        this.pname = pname;
+        this.price = price;
+        this.quantity = quantity;
+        this.discount = discount;*/
     }
 
     public String getName() {
